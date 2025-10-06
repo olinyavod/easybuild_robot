b2fd6148a773d80cb8cadb66b2398b20d08ea9f4
olinyavod <olinyavod@gmail.com>
2025-10-06 10:19:51 +0000

    Support .env file was added

--- bin/main.dart
+++ bin/main.dart
@@ -2,19 +2,28 @@ import 'dart:io';
 import 'package:easybuild_bot/hive_registrar.g.dart';
 import 'package:hive_ce/hive.dart';
 import 'package:hotreloader/hotreloader.dart';
+import 'package:dotenv/dotenv.dart';
 
 import 'package:easybuild_bot/easybuild_bot.dart';
 import 'package:easybuild_bot/di/service_locator.dart';
 
 // This is a general example of how to use the Televerse library.
 void main(List<String> args) async {
+  // Загружаем переменные окружения из .env файла
+  var env = DotEnv();
+  env.load();
+
   // Initialize Hive
+  final hiveDir = Directory('${Directory.current.path}/data/hive');
+  if (!await hiveDir.exists()) {
+    await hiveDir.create(recursive: true);
+  }
   Hive
-    ..init(Directory.current.path)
+    ..init(hiveDir.path)
     ..registerAdapters();
 
   // Регистрируем токен бота как именованную зависимость
-  final String token = Platform.environment["BOT_TOKEN"]!;
+  final String token = env['BOT_TOKEN'] ?? Platform.environment["BOT_TOKEN"]!;
   getIt.registerSingleton<String>(token, instanceName: 'botToken');
 
   // Инициализируем DI контейнер
